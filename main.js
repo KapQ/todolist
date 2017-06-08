baf2d0f8adc88e135eed041278b95e244e7e4cf1
KapQ <kquarles18@gmail.com>
2017-06-08 15:26:43 -0400

    Lets See

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-// Add your JS here.
+// Add your JS here
+let todoapp = document.querySelector('todoapp');
+let header = document.querySelector('header');
+let new-todo = document.querySelector('newtodo');
+let body = document.querySelectorAll('body');
+let main = document.querySelector('main');
+let todo-list = document.querySelector('todolist');
+todolist.addEventListener('keypress', function addListItem(event) {
+  if (event.keyCode == 13){
+    let todoContent = event.target.value;
+    event.target.value = "";
+    let newTodoli = document.createElement('li');
+    newTodoli.textContent = todoContent;
+    todoList.appendChild(newTodoli);
+    todolist.innerHTML = `<li><div class="view"><label>Buy Milk</label></div></li>`;
+    todoList.appendChild(newTodoli);
+    todolist.innerHTML = `<li><div class="view"><label>Wash Car</label></div></li>`
+  }
+    return event;
+  }
+});
